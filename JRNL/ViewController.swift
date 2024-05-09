f2bff55a379d19f5e6c39ad4f9a9f4f072931f18
Jin Park <jinpark1504@gmail.com>
2024-05-09 14:29:56 +0900

    JRNL initial interface

---
+++ JRNL/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JRNL
+//
+//  Created by Jungjin Park on 2024-05-09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
